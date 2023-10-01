f0672a43cbfb9dacbc212d7477b7f3a89d35b7f3
fcosoftware <fcosoftware35@gmail.com>
2023-10-01 00:26:58 -0300

    Uso do reduce e do filter no projeto

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
 const list = document.querySelector("ul")
 const buttonShowAll = document.querySelector(".show-all")
 const buttonMapAll = document.querySelector(".map-all")
+const buttonReduceAll = document.querySelector(".sum-all")
+const buttonFilterAll = document.querySelector(".filter-all")
 
 //let myLi = ""
+
+function formatCurrency(value) {
+    const newValue = value.toLocaleString('pt-br',
+    {style: 'currency', 
+    currency: 'BRL'
+});
+    return newValue
+}
 
 function showAll(productsArrays) {
 
@@ -14,7 +24,7 @@ function showAll(productsArrays) {
             <li>
                 <img src=${product.src}>
                 <p>${product.name}</p>
-                <p class="item-price">R$ ${product.price}</p>
+                <p class="item-price">R$ ${formatCurrency(product.price)}</p>
             </li>
             
             `
@@ -38,6 +48,27 @@ function mapAllItems() {
     //console.log(newPrice)
 }
 
+function sumAllItems() {
+   const totalValue = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+   
+   list.innerHTML = `
+   
+            <li>
+                <p>O valor total dos Itens é R$ ${formatCurrency(totalValue)}</p>        
+            </li>
+        `
+}
+
+function filterAllItems() {
+    const filterJustVegan = menuOptions.filter((product) => product.vegan === true)
+
+    showAll(filterJustVegan)
+}
+
 buttonShowAll.addEventListener("click", () => showAll(menuOptions))
 
 buttonMapAll.addEventListener("click", mapAllItems)
+
+buttonReduceAll.addEventListener("click", sumAllItems)
+
+buttonFilterAll.addEventListener("click", filterAllItems)
